fa7ff2d7912056edc4d8218dc226b891589f305d
Wyatt <47566951+WyThatGuy@users.noreply.github.com>
2024-07-10 23:49:47 -0700

    tweak no.3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var frame = document.getElementById("frame")
 
 window.onload = function () {
-    if (location.href.includes("#")) {
-        if (location.href.split("#", 2)[1].includes("https://") || location.href.split("#", 2)[1].includes("https://")) {
-            frame.src = location.href.split("#", 1)[1]
+    if (window.location.href.includes("#")) {
+        if (window.location.href.split("#", 2)[1].includes("https://") || window.location.href.split("#", 2)[1].includes("https://")) {
+            frame.src = window.location.href.split("#", 1)[1]
         } else {
-            frame.src = "http://" + location.href.split("#", 1)[1]
+            frame.src = "http://" + window.location.href.split("#", 2)[1]
         }
     } else {
         frame.srcdoc = "<h1>Welcome to BetterView!</h1>\n<p>Add a # (hashtag) and then a link to the end of the URL above to access a website</P>"
