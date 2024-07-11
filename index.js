6d6e54582fa42a4088fb6b4a25c3cfd5a82b02f5
Wyatt <47566951+WyThatGuy@users.noreply.github.com>
2024-07-10 23:54:41 -0700

    final tweak

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.onload = function () {
         if (window.location.href.split("#", 2)[1].includes("https://") || window.location.href.split("#", 2)[1].includes("https://")) {
             frame.src = window.location.href.split("#", 2)[1]
         } else {
-            frame.src = "http://" + window.location.href.split("#", 2)[1]
+            frame.src = "https://" + window.location.href.split("#", 2)[1]
         }
     } else {
         frame.srcdoc = "<h1>Welcome to BetterView!</h1>\n<p>Add a # (hashtag) and then a link to the end of the URL above to access a website</P>"
